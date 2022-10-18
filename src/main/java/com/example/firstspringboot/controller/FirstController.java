df7f7aa5fc93c407260e091d81244db48f0e32d9
Geunhak Lee <rmsgkr2@gmail.com>
2022-10-18 14:17:04 +0900

    feat: MVC sample, layout

---
+++ src/main/java/com/example/firstspringboot/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.example.firstspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("userName", "근학");
+        return "greetings";
+    }
+}
